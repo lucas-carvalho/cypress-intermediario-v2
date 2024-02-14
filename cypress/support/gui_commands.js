815821b9e0fce7c854ffeb0c0d16d6064cc01dda
Lucas Carvalho <lucascarvalhosilva@outlook.com>
2024-02-14 18:05:14 -0300

    Update quotation to single quote

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,49 +1,51 @@
 Cypress.Commands.add(
-  "login",
+  'login',
   (
-    user = Cypress.env("user_name"),
-    password = Cypress.env("user_password"),
+    user = Cypress.env('user_name'),
+    password = Cypress.env('user_password'),
     { cacheSession = true } = {}
   ) => {
     const login = () => {
-      cy.visit("/users/sign_in");
+      cy.visit('/users/sign_in')
 
-      cy.get("[data-qa-selector='login_field']").type(user);
+      cy.get("[data-qa-selector='login_field']").type(user)
       cy.get("[data-qa-selector='password_field']").type(password, {
         log: false,
-      });
-      cy.get("[data-qa-selector='sign_in_button']").click();
-    };
+      })
+      cy.get("[data-qa-selector='sign_in_button']").click()
+    }
 
     const validate = () => {
-    cy.visit('/')
-    cy.location('pathname', { timeout: 1000 })
-      .should('not.eq', '/users/sign_in')
-  }
+      cy.visit('/')
+      cy.location('pathname', { timeout: 1000 }).should(
+        'not.eq',
+        '/users/sign_in'
+      )
+    }
 
     const options = {
       cacheAcrossSpecs: true,
-      validate
-    };
+      validate,
+    }
 
     if (cacheSession) {
-      cy.session(user, login, options);
+      cy.session(user, login, options)
     } else {
-      login();
+      login()
     }
   }
-);
+)
 
-Cypress.Commands.add("logout", () => {
-  cy.get(".qa-user-avatar").click();
-  cy.get("[data-qa-selector='sign_out_link']").click();
-});
+Cypress.Commands.add('logout', () => {
+  cy.get('.qa-user-avatar').click()
+  cy.get("[data-qa-selector='sign_out_link']").click()
+})
 
-Cypress.Commands.add("gui_create_project", (project) => {
-  cy.visit("/projects/new");
+Cypress.Commands.add('gui_create_project', (project) => {
+  cy.visit('/projects/new')
 
-  cy.get("#project_name").type(project.name);
-  cy.get("#project_description").type(project.description);
-  cy.get(".qa-initialize-with-readme-checkbox").check();
-  cy.contains("Create project").click();
-});
+  cy.get('#project_name').type(project.name)
+  cy.get('#project_description').type(project.description)
+  cy.get('.qa-initialize-with-readme-checkbox').check()
+  cy.contains('Create project').click()
+})
